731df2a48704b19f23d201079bd1d1b498cd631a
The Hard Gamer V@RuN <gamelovr695@gmail.com>
2017-12-28 16:33:47 +0530

    Update and rename validus.mk to aosp_a6000.mk

--- aosp_a6000.mk
+++ aosp_a6000.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/lenovo/a6000/full_a6000.mk)
 
 # Inherit some common ValidusOS stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -25,7 +25,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm8916,Kraft-A6000,wt86518
 
-PRODUCT_NAME := validus_a6000
+PRODUCT_NAME := aosp_a6000
 BOARD_VENDOR := Lenovo
 
 # Build fingerprint
